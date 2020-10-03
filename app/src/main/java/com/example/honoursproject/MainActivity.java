3b40c0ac606897a573521dea0e65627ac854b218
Conner McGill <43885110+mcgillc10@users.noreply.github.com>
2020-10-03 18:34:48 +0100

    Added toolbar and tweaked main activity

--- app/src/main/java/com/example/honoursproject/MainActivity.java
+++ app/src/main/java/com/example/honoursproject/MainActivity.java
@@ -13,19 +13,23 @@ properly. I will update this in due course
 
 package com.example.honoursproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button logoutButton;
     private FirebaseAuth mAuth;
 
     @Override
@@ -33,17 +37,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        logoutButton = findViewById(R.id.button_logout);
-
         mAuth = FirebaseAuth.getInstance();
 
-        logoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                logoutUserFromAccount();
-            }
-        });
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                logoutUserFromAccount();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void logoutUserFromAccount() {
